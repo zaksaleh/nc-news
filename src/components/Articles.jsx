2066d3edf39ad66b23a416976e9aab143204204d
Zak Saleh <zaksaleh@sky.com>
2023-04-18 11:10:55 +0100

    view-all-articles-2

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -4,23 +4,26 @@ import { useState, useEffect } from "react";
 
 const Articles = () => {
   const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
+    setLoading(true);
     fetchArticles().then((articles) => {
       setArticles(articles);
+      setLoading(false);
     });
   }, []);
 
-  return (
-    <div className="Articles">
-      <ul>
-        <li>
-          {articles.map((article) => {
-            return <ArticleListCard key={article.article_id} {...article} />;
-          })}
-        </li>
-      </ul>
-    </div>
+  return loading ? (
+    <h2>Loading...</h2>
+  ) : (
+    <main className="Articles">
+      <li>
+        {articles.map((article) => {
+          return <ArticleListCard key={article.article_id} {...article} />;
+        })}
+      </li>
+    </main>
   );
 };
 
