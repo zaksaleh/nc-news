d9d8d64e0ec592e937cf4596dc8e12749f11ac28
Zak Saleh <zaksaleh@sky.com>
2023-06-14 11:47:27 +0100

    loading-screen-center

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -3,7 +3,9 @@ import CircularProgress from "@mui/material/CircularProgress";
 const Loading = () => {
   return (
     <main className="loading-layout">
-      <CircularProgress color="secondary" className="Loading-icon" />
+      <section className="loading-flex">
+        <CircularProgress color="secondary" className="loading-icon" />
+      </section>
     </main>
   );
 };
